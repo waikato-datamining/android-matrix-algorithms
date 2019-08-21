7905b081831a014036ea6a16d1d12c9bf5b4f4d2
csterling <coreytsterling@gmail.com>
2019-08-21 13:22:58 +1200

    Added unit tests for SavitzkyGolay, SIMPLS.

---
+++ src/test/java/com/github/waikatodatamining/androidmatrix/SIMPLSTest.java
@@ -0,0 +1,48 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * SIMPLSTest.java
+ * Copyright (C) 2019 University of Waikato, Hamilton, NZ
+ */
+
+package com.github.waikatodatamining.androidmatrix;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.FileInputStream;
+
+/**
+ * Tests the SIMPLS algorithm.
+ *
+ * @author Corey Sterling (csterlin at waikato dot ac dot nz)
+ */
+public class SIMPLSTest {
+
+  @Test
+  public void apply() throws Exception {
+    SIMPLS simpls = new SIMPLS(new FileInputStream("src/test/resources/com/github/waikatodatamining/androidmatrix/SIMPLS.dat"));
+
+    double[][] bolts = PyMADeserialisation.deserialiseMatrix(new FileInputStream("src/test/resources/com/github/waikatodatamining/androidmatrix/bolts.dat"));
+
+    double[][] expectedResponse = PyMADeserialisation.deserialiseMatrix(new FileInputStream("src/test/resources/com/github/waikatodatamining/androidmatrix/SIMPLS-bolts.dat"));
+
+    for (int i = 0; i < bolts.length; i++)
+      Assertions.assertArrayEquals(expectedResponse[i], simpls.apply(bolts[i]), 1e-15);
+
+  }
+
+}
